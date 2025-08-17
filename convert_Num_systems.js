2a881730c5b0f187de708cc62820ab04421285f4
Woodlandsyou <linus_hahlen@web.de>
2025-08-17 21:41:16 +0200

    made it for all bases

--- convert_Num_systems.js
+++ convert_Num_systems.js
@@ -1,7 +1,6 @@
 const [start_sys, target_sys, _num] = (() => {
     const [ArgsString] = process.argv.slice(2);
-    array = ArgsString.split(/[ ,]/);
-    return array;
+    return ArgsString.split(/[ ,]/);
 })()
 const symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
 if(_num <= 0) throw new Error("Number must be over 0");
@@ -11,60 +10,22 @@ if(_num <= 0) throw new Error("Number must be over 0");
     }
 })();
 
-// const result = (function convert() {
-//     if(start_sys === "BIN") {
-//         if(target_sys === "DEC") {
-//             let converted = 0;
-//             for (let i = 0; i < _num.length; i++) {
-//                 converted += parseInt(_num[i]) * Math.pow(2, _num.length - 1 - i);
-//             }
-//             let result = `DEC ${converted}`;
-//             return result;
-//         }
-//     } else if(start_sys === "DEC") {
-//         if(target_sys === "BIN") {
-//             let converted = [];
-//             (function takeLogs(num) {
-//                 if(num === 0) return;
-//                 let temp = Math.floor(Math.log2(num));
-//                 converted.push(temp);
-//                 takeLogs(num - Math.pow(2, temp));
-//             })(_num);
-
-//             let result = "BIN ";
-//             let index = 0;
-//             for (let i = 0; i <= converted[0]; i++) {
-//                 if(i === converted[0] - converted[index]) {
-//                     result += 1;
-//                     index++;
-//                 }
-//                 else result += 0;
-//             }
-//             return result;
-//         } else if(target_sys === "HEX") {
-//             let converted = [];
-//             (function takeLogs(num) {
-//                 if(num === 0) return;
-//                 let temp = Math.floor(log(16, num));                
-//                 converted.push(temp);
-//                 takeLogs(num - Math.pow(16, temp));
-//             })(_num);
-
-//             let result = "HEX ";
-//             return [result, converted];
-//         }
-//     }
-// })();
 const result = convert(start_sys, target_sys, _num);
 console.log(result);
 
 function convert(start, target, num) {
-    let converted = [], dec = 0;
+    let converted = [], dec = 0, result = "";
 
-    for (let i = 0; i < num.length; i++) {
-        dec += symbols.indexOf(num[i]) * Math.pow(start, num.length - 1 - i);     
-    }
-    return dec;
+    for (let i = 0; i < num.length; i++) dec += symbols.indexOf(num[i]) * Math.pow(start, num.length - 1 - i);
+
+    (function takeLogs(num) {
+        if(num === 0) return;
+        let temp = Math.floor(log(target, num));                
+        converted.push(temp);
+        takeLogs(num - Math.pow(target, temp));
+    })(dec);
+
+    return countElements(converted);
 }
 
 function log(n, m) {
@@ -78,7 +39,6 @@ function countElements(array) {
     for (const element of array) {
         if(element === prev) {
             symbolIndex++;
-            if(symbolIndex >= symbols.length) throw new Error("Same Powers can't occur more often then there are symbols for each base. Please check your input!");
             count[count.length - 1] = symbols[symbolIndex];
         } else {
             symbolIndex = 1;
